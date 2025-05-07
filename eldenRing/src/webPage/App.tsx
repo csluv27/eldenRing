b9e993451fda4088e9da1e441d5d92dc908faf24
CSLuv <120657195+csluv27@users.noreply.github.com>
2025-05-07 23:23:37 +0700

    tìm thêm một số ảnh

--- eldenRing/src/webPage/App.tsx
+++ eldenRing/src/webPage/App.tsx
@@ -1,26 +1,56 @@
-import { Parallax, ParallaxLayer } from '@react-spring/parallax';
+import './App.css';
+import { useState, useEffect } from 'react';
 
 export default function App() {
+
+  const [showTop, setShowTop] = useState(true);
+  useEffect(() => {
+    const handleWheel = (e: { deltaY: number; }) => {
+      if (e.deltaY > 0) {
+        setShowTop(false); // Cuộn xuống => Ẩn header
+      } else if (e.deltaY < 0) {
+        setShowTop(true); // Cuộn lên => Hiện header
+      }
+    };
+    window.addEventListener('wheel', handleWheel);
+    return () => window.removeEventListener('wheel', handleWheel);
+  }, []);
+
   return (
-    <div className="h-screen w-screen overflow-hidden">
-      <Parallax pages={3} className="relative">
+    <section className="relative w-full h-[200vh] overflow-x-hidden">
+      {/* VIDEO NỀN FIXED */}
+      <div className={`transition-transform duration-200 ease-in-out ${showTop ? 'translate-y-0' : '-translate-y-3'}`}>
 
-        {/* VIDEO BACKGROUND */}
-        <ParallaxLayer offset={0} factor={1} speed={0.1}>
-          <video
-            src="./videoChude.mp4"
-            autoPlay
-            muted
-            loop
-            className="w-full h-screen object-contain scale-115"
-          />
-        </ParallaxLayer>
+        <video
+          src="./videoChude.mp4"
+          autoPlay
+          muted
+          loop
+          className="fixed mt-32 w-screen h-fit overflow-clip  object-cover z-0 pointer-events-none"
+        />
+      </div>
 
+
+      
+      <div
+        className="z-[-1]  h-full  flex w-screen items-end justify-center"
+        style={{
+          backgroundImage: "url('/erbackground2.jpg')",
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
        
+        }}
+      >
+      <img
+        src="./eldenring_new.png"
+        alt="Elden Ring"
+        className="w-full max-w-4xl object-contain z-11 pointer-events-none absolute top-170"
+      />
+      </div>
 
-       
 
-      </Parallax>
-    </div>
+
+    </section >
   );
 }
+
