7d1f1154b77592f14f6e49fc6161a4d892072d08
CSLuv <120657195+csluv27@users.noreply.github.com>
2025-05-08 21:35:43 +0700

    chỉnh được đầu trang dưới

--- eldenRing/src/webPage/App.tsx
+++ eldenRing/src/webPage/App.tsx
@@ -1,56 +1,48 @@
+
 import './App.css';
-import { useState, useEffect } from 'react';
 
 export default function App() {
 
-  const [showTop, setShowTop] = useState(true);
-  useEffect(() => {
-    const handleWheel = (e: { deltaY: number; }) => {
-      if (e.deltaY > 0) {
-        setShowTop(false); // Cuộn xuống => Ẩn header
-      } else if (e.deltaY < 0) {
-        setShowTop(true); // Cuộn lên => Hiện header
-      }
-    };
-    window.addEventListener('wheel', handleWheel);
-    return () => window.removeEventListener('wheel', handleWheel);
-  }, []);
 
   return (
-    <section className="relative w-full h-[200vh] overflow-x-hidden">
+    <section className="section-container relative w-full h-[200vh] overflow-x-hidden">
       {/* VIDEO NỀN FIXED */}
-      <div className={`transition-transform duration-200 ease-in-out ${showTop ? 'translate-y-0' : '-translate-y-3'}`}>
-
+      <div className="video-fixed relative">
         <video
           src="./videoChude.mp4"
           autoPlay
           muted
           loop
-          className="fixed mt-32 w-screen h-fit overflow-clip  object-cover z-0 pointer-events-none"
+          className="w-screen fixed mt-30 h-auto object-cover pointer-events-none"
         />
       </div>
 
-
-      
       <div
-        className="z-[-1]  h-full  flex w-screen items-end justify-center"
+        className="z-2 h-full flex relative mt-201 w-screen items-end justify-center relative-content"
         style={{
           backgroundImage: "url('/erbackground2.jpg')",
           backgroundSize: 'cover',
           backgroundPosition: 'center',
-       
         }}
       >
-      <img
-        src="./eldenring_new.png"
-        alt="Elden Ring"
-        className="w-full max-w-4xl object-contain z-11 pointer-events-none absolute top-170"
-      />
+        
+
+        <img
+          src="./eldenring_new.png"
+          alt="Elden Ring"
+          className="w-full max-w-3xl z-2 object-contain pointer-events-none absolute top-[-120px] eldenring-background"
+        />
+        <div className="text-white  absolute top-40 text-center z-2 flex flex-col">
+          <h2 className='font-bold text-5xl mb-6'>ELDEN RING</h2>
+          <div className='flex flex-row gap-8 text-[13px]'>
+            <span><span className='text-[#c19d53]'>Release Date:</span> 25/02/2022</span>
+            <span><span className='text-[#c19d53]'>Genres:</span> RPG</span>
+            <span><span className='text-[#c19d53]'>Developer:</span> FromSoftware, Inc</span>
+
+          </div>
+        </div>
+   
       </div>
-
-
-
-    </section >
+    </section>
   );
 }
-
